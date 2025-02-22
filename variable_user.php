74f5d35b421e7d348a44ea0b41c93eb9bd61969d
mlndrptr <0110124132@student.nurulfikri.ac.id>
2025-02-22 13:47:38 +0700

    upload tugas

---
+++ variable_user.php
@@ -0,0 +1,10 @@
+<?php
+// definisikan variables
+$nama = 'Mulandari Putri';
+$umur = 19;
+$berat = 42;
+echo 'Nama : ' . $nama;
+echo '<br/>Umur : ' . $umur.' Tahun';
+echo '<br/>Berat : '.$berat.' Kg';
+echo "<br/>Hello $nama Apa kabar?";
+?>
