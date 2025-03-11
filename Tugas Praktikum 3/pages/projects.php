5d9a498ef5b6b064a13b3ea48e0df6a9c0abaf2c
mlndrptr <0110124132@student.nurulfikri.ac.id>
2025-03-12 00:43:51 +0700

    upload tugas pertemuan 03

---
+++ Tugas Praktikum 3/pages/projects.php
@@ -0,0 +1,58 @@
+<!-- Projects Section-->
+            <section class="py-5">
+                <div class="container px-5 mb-5">
+                    <div class="text-center mb-5">
+                        <h1 class="display-5 fw-bolder mb-0"><span class="text-gradient d-inline">Projects</span></h1>
+                    </div>
+                    <div class="row gx-5 justify-content-center">
+                        <div class="col-lg-11 col-xl-9 col-xxl-8">
+                            <!-- Project Card 1-->
+                            <div class="card overflow-hidden shadow rounded-4 border-0 mb-5">
+                                <div class="card-body p-0">
+                                    <div class="d-flex align-items-center">
+                                        <div class="p-5">
+                                            <h2 class="fw-bolder">Project Akhir DDP</h2>
+                                            <p>Membuat aplikasi tentang perkiraan cuaca menggunakan bahasa pemrograman python (streamlit), menggunakan API untuk mengambil data secara real-time.</p>
+                                        </div>
+                                        <img class="img-fluid" src="assets/ddp.jpeg" alt="..." />
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Project Card 2-->
+                            <div class="card overflow-hidden shadow rounded-4 border-0 mb-5">
+                                <div class="card-body p-0">
+                                    <div class="d-flex align-items-center">
+                                        <div class="p-5">
+                                            <h2 class="fw-bolder">Project Akhir PemWeb1</h2>
+                                            <p>Membuat landing page "cafe" menggunakan html, css, dan javascript.</p>
+                                        </div>
+                                        <img class="img-fluid" src="assets/pw1.jpeg" alt="..." />
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Project Card 2-->
+                            <div class="card overflow-hidden shadow rounded-4 border-0">
+                                <div class="card-body p-0">
+                                    <div class="d-flex align-items-center">
+                                        <div class="p-5">
+                                            <h2 class="fw-bolder">Project Akhir UJIKOM</h2>
+                                            <p>Membuat project "pembayaran spp" menggunakan bahasa pemrograman PHP saat SMK untuk penilaian uji kompetensi keahlian.</p>
+                                        </div>
+                                        <img class="img-fluid" src="assets/ujikom.jpeg" alt="..." />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- Call to action section-->
+            <section class="py-5 bg-gradient-primary-to-secondary text-white">
+                <div class="container px-5 my-5">
+                    <div class="text-center">
+                        <h2 class="display-4 fw-bolder mb-4">Let's build something together</h2>
+                        <a class="btn btn-outline-light btn-lg px-5 py-3 fs-6 fw-bolder" href="?page=contact">Contact me</a>
+                    </div>
+                </div>
+            </section>
+        </main>
---
+++ Tugas Praktikum 3/pages/resume.php
@@ -0,0 +1,131 @@
+            <!-- Page Content-->
+            <div class="container px-5 my-5">
+                <div class="text-center mb-5">
+                    <h1 class="display-5 fw-bolder mb-0"><span class="text-gradient d-inline">Resume</span></h1>
+                </div>
+                <div class="row gx-5 justify-content-center">
+                    <div class="col-lg-11 col-xl-9 col-xxl-8">
+                        <!-- Experience Section-->
+                        <section>
+                            <div class="d-flex align-items-center justify-content-between mb-4">
+                                <h2 class="text-primary fw-bolder mb-0">Pengalaman</h2>
+                                <!-- Download resume button-->
+                                <!-- Note: Set the link href target to a PDF file within your project
+                                <a class="btn btn-primary px-4 py-3" href="#!">
+                                    <div class="d-inline-block bi bi-download me-2"></div>
+                                    Download Resume
+                                </a>-->
+                            </div>
+                            <!-- Experience Card 1-->
+                            <div class="card shadow border-0 rounded-4 mb-5">
+                                <div class="card-body p-5">
+                                    <div class="row align-items-center gx-5">
+                                        <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                                            <div class="bg-light p-4 rounded-4">
+                                                <div class="text-primary fw-bolder mb-2">2024</div>
+                                                <div class="small fw-bolder">Kasir</div>
+                                                <div class="small text-muted">Botani Bakery</div>
+                                                <div class="small text-muted">Dramaga, Kab. Bogor</div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-8"><div>Melayani pelanggan, mengurus transaksi, menjaga stok barang/opname, merekap laporan penjualan. </div></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Experience Card 2-->
+                            <div class="card shadow border-0 rounded-4 mb-5">
+                                <div class="card-body p-5">
+                                    <div class="row align-items-center gx-5">
+                                        <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                                            <div class="bg-light p-4 rounded-4">
+                                                <div class="text-primary fw-bolder mb-2">2022 - 2023</div>
+                                                <div class="small fw-bolder">Praktik Kerja Industri</div>
+                                                <div class="small text-muted">DISKOMINFO</div>
+                                                <div class="small text-muted">Kota Bogor</div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-8"><div>Berkontribusi membuat aplikasi web kebencanaan menggunakan laravel, menginput ± 7000 data, membuat permainan aplikasi sederhana menggunakan godot, mempelajari digital marketing.</div></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                        <!-- Education Section-->
+                        <section>
+                        <h2 class="text-secondary fw-bolder mb-4">Pendidikan</h2>                             
+<!-- Education Card 1-->                             
+<div class="card shadow border-0 rounded-4 mb-5">                                 
+    <div class="card-body p-10">                                     
+        <div class="row align-items-center gx-5">                                         
+            <div class="col text-center text-lg-start mb-4 mb-lg-0">                                             
+                <div class="bg-light p-4 rounded-4">                                                 
+                    <div class="text-secondary fw-bolder mb-2">2024 - Sekarang</div>                                                 
+                    <div class="mb-2">                                                                                               
+                        <div class="small text-muted">Jagakarsa, Jakarta Selatan</div>                                                 
+                    </div>                                            
+                </div>                                         
+            </div>                                         
+            <div class="col-lg-8"><div><b>STT Terpadu Nurul Fikri - Sistem Informasi</b></div></div>                                     
+        </div>                                 
+    </div>                             
+</div>                             
+<!-- Education Card 2-->                             
+<div class="card shadow border-0 rounded-4 mb-5">                                 
+    <div class="card-body p-10">                                     
+        <div class="row align-items-center gx-5">                                         
+            <div class="col text-center text-lg-start mb-4 mb-lg-0">                                             
+                <div class="bg-light p-4 rounded-4">                                                 
+                    <div class="text-secondary fw-bolder mb-2">2020 - 2023</div>                                                 
+                    <div class="mb-2">                                                   
+                        <div class="small text-muted">Ciomas, Kab. Bogor</div>                                                 
+                    </div>                                            
+                </div>                                         
+            </div>                                         
+            <div class="col-lg-8"><div><b>SMKN 1 Ciomas - Rekayasa Perangkat Lunak</b></div></div>                                     
+        </div>                                 
+    </div>                             
+</div>
+
+                        </section>
+                        <!-- Divider-->
+                        <div class="pb-5"></div>
+                        <!-- Skills Section-->
+                        <section>
+                            <!-- Skillset Card-->
+                            <div class="card shadow border-0 rounded-4 mb-5">
+                                <div class="card-body p-5">
+                                    <!-- Professional skills list-->
+                                    <div class="mb-5">
+                                        <div class="d-flex align-items-center mb-4">
+                                            <div class="feature bg-primary bg-gradient-primary-to-secondary text-white rounded-3 me-3"><i class="bi bi-tools"></i></div>
+                                            <h3 class="fw-bolder mb-0"><span class="text-gradient d-inline">Professional Skills</span></h3>
+                                        </div>
+                                        <div class="row row-cols-1 row-cols-md-3 mb-4">
+                                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Web Development</div></div>
+                                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Data Entry</div></div>
+                                            <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Digital Marketing</div></div>
+                                        </div>
+                                    </div>
+                                    <!-- Languages list-->
+                                    <div class="mb-0">
+                                        <div class="d-flex align-items-center mb-4">
+                                            <div class="feature bg-primary bg-gradient-primary-to-secondary text-white rounded-3 me-3"><i class="bi bi-code-slash"></i></div>
+                                            <h3 class="fw-bolder mb-0"><span class="text-gradient d-inline">Bahasa Pemrograman</span></h3>
+                                        </div>
+                                        <div class="row row-cols-1 row-cols-md-3 mb-4">
+                                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">HTML</div></div>
+                                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">CSS</div></div>
+                                            <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">JavaScript</div></div>
+                                        </div>
+                                        <div class="row row-cols-1 row-cols-md-3">
+                                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Python</div></div>
+                                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">PHP</div></div>
+                                            <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">C++</div></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </main>
